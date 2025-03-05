5b65ef592f9c770b34712a0e5c88d34c0c45c81a
Prakaikool <prakaikool.teepraken@gmail.com>
2025-03-05 08:40:31 +0100

    Import profile picture

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,12 +4,10 @@
   <div class="container">
     <h1>PRAKAIKOOL TEEPRAKEN</h1>
     <h2>Frontend developer student</h2>
-    <p>
-      A patient, adaptable, and creative frontend development student with experience in building
-      responsive web applications. Proficient in HTML, CSS, JavaScript, and modern frameworks.
-      Passionate about delivering user-friendly solutions and high-quality user experiences. Enjoys
-      teamwork and collaboration and is always eager to learn and grow.
-    </p>
+
+    <button @click="Hello">Resume</button>
+
+    <img src="/images/profile_picture.png" alt="Profile picture" />
   </div>
 </template>
 
