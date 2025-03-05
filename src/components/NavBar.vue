634aed717acacf47ca0a5952d89a302e044bda3f
Prakaikool <prakaikool.teepraken@gmail.com>
2025-03-05 10:24:26 +0100

    Create navbar

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,5 +1,29 @@
-<script setup></script>
+<script setup>
+import { RouterLink } from 'vue-router'
+import { ref } from 'vue'
+
+const menuOpen = ref(false)
+const toggleMenu = () => {
+  menuOpen.value = !menuOpen.value
+}
+</script>
+
 <template>
-  <div></div>
+  <nav class="navbar">
+    <div class="logo">Prakaikool Teepraken</div>
+
+    <div class="hambuger" @click="toggleMenu">
+      <span :class="{ open: menuOpen }"></span>
+      <span :class="{ open: menuOpen }"></span>
+      <span :class="{ open: menuOpen }"></span>
+    </div>
+
+    <div class="nav-links" :class="{ active: menuOpen }">
+      <router-link class="link" to="/" active-class="isActive">Home</router-link>
+      <router-link class="link" to="/projects" active-class="isActive">Projects</router-link>
+      <router-link class="link" to="/contact" active-class="isActive">Contact</router-link>
+    </div>
+  </nav>
 </template>
+
 <style scoped></style>
