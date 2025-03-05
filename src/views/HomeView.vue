142f0c52d439c89c75a50693e1b1fa0d9a95581a
Prakaikool <prakaikool.teepraken@gmail.com>
2025-03-05 09:37:49 +0100

    Add datas

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -5,9 +5,60 @@
     <h1>PRAKAIKOOL TEEPRAKEN</h1>
     <h2>Frontend developer student</h2>
 
-    <button @click="Hello">Resume</button>
+    <button @click="downLoadFile">Resume</button>
 
     <img src="/images/profile_picture.png" alt="Profile picture" />
+
+    <p>Get to know more</p>
+    <h2>About me</h2>
+
+    <img src="" alt="" />
+
+    <div class="education-program-box">
+      <div class="education">
+        <h3>Education</h3>
+        <p>IT-högskolan</p>
+        <p>Järfälla gymnasium</p>
+      </div>
+
+      <div class="program">
+        <h3>Program</h3>
+        <p>Frontend Developer</p>
+        <p>Arts program</p>
+      </div>
+
+      <p>
+        A patient, adaptable, and creative frontend development student with experience in building
+        responsive web applications. Proficient in HTML, CSS, JavaScript, and modern frameworks.
+        Passionate about delivering user-friendly solutions and high-quality user experiences.
+        Enjoys teamwork and collaboration and is always eager to learn and grow.
+      </p>
+    </div>
+
+    <div>
+      <h3>Latest projects</h3>
+      <img src="" alt="" />
+      <h3>Projects name</h3>
+
+      <img src="" alt="" />
+      <h3>Projects name</h3>
+
+      <h3>More</h3>
+    </div>
+
+    <div>
+      <h3>Frontend development</h3>
+      <p>Skills and tools</p>
+      <ul>
+        <li>HTML</li>
+        <li>CSS</li>
+        <li>Java Script</li>
+      </ul>
+    </div>
+
+    <div>
+      <h3>Hobby</h3>
+    </div>
   </div>
 </template>
 
