0fc97d9ade6450e64e8bbe032d407a4e10ac0789
Prakaikool <prakaikool.teepraken@gmail.com>
2025-03-05 08:19:32 +0100

    Clear junk files

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,16 @@
-<script setup>
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
+<script setup></script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <div class="container">
+    <h1>PRAKAIKOOL TEEPRAKEN</h1>
+    <h2>Frontend developer student</h2>
+    <p>
+      A patient, adaptable, and creative frontend development student with experience in building
+      responsive web applications. Proficient in HTML, CSS, JavaScript, and modern frameworks.
+      Passionate about delivering user-friendly solutions and high-quality user experiences. Enjoys
+      teamwork and collaboration and is always eager to learn and grow.
+    </p>
+  </div>
 </template>
+
+<style scoped></style>
